10b4eccde022a6f66f29b1774fd2a17fdf06a0a1
Sergey Sorokin <s.sorokin@prodege.com>
2014-03-25 17:08:21 +0400

    Version 1.0.3

--- inheritance.js
+++ inheritance.js
@@ -1,5 +1,5 @@
 /**
- * @version 1.0.2
+ * @version 1.0.3
  * @throws {Error}
  */
 
@@ -62,37 +62,57 @@
          * Parse arguments for the Class.create() method.
          *
          * @param {object} args Arguments.
-         * @param {ojbect} An object like {parent, props}
+         * @param {object} An object like {parent, props}.
          */
         parseArgs: function (args) {
-            var parent = null,
-                props = {};
+            var parsedArgs = {};
 
-            // Class.create(parent|props)
+            // parent|props
             if (args.length === 1) {
                 if (typeof args[0] === 'function') {
-                    parent = args[0];
+                    parsedArgs.parent = args[0];
                 } else if (typeof args[0] === 'object') {
-                    props = args[0];
+                    parsedArgs.props = args[0];
                 } else {
                     throw new TypeError('Invalid arguments');
                 }
             }
 
-            // Class.create(parent, props)
+            // parent, props
             if (args.length >= 2) {
                 if (typeof args[0] !== 'function' || typeof args[1] !== 'object') {
                     throw new TypeError('Invalid arguments');
                 }
 
-                parent = args[0];
-                props = args[1];
+                parsedArgs.parent = args[0];
+                parsedArgs.props = args[1];
             }
 
-            return {
-                parent: parent,
-                props: props
-            };
+            return parsedArgs;
+        },
+
+        /**
+         * Inherit function.
+         *
+         * @param {function} parent Parent class.
+         * @param {function} child Child class.
+         * @return {function} inherited class.
+         */
+        inherit: function (parent, child) {
+            // basic inherit      
+            child.prototype = Helper.createObject(parent.prototype);
+
+            // fix the constructor
+            if (child.prototype.constructor === Object.prototype.constructor) {
+                child.prototype.constructor = child;
+            }
+
+            // keep the superclass reference
+            child.super = parent.prototype;
+
+            // child.prototype.parent = child;
+
+            return child;
         }
     };
 
@@ -130,62 +150,36 @@
      *     });
      * })();
      */
-    Global.Class = (function () {
-        /** @constructor */
-        var ClassWrapper = function (parent) {
-            var hasParent = typeof parent !== 'undefined' && parent !== null;
+    Global.Class = {
+        /** 
+         * Create a class.
+         * Parameters can be as follows:
+         *   - parent is a parent class
+         *   - props is an object which contains class properties
+         *
+         * @param {function|object|null} The parameter can be parent or props or null. 
+         * @param {object} The parameter is props (optional).
+         * @return {function} A function which can be used as a class.
+         * @throws {TypeError}
+         */
+        create: function () {
+            var args = Helper.parseArgs(arguments),
+                parent = args.parent || null,
+                props = args.props || {},
+                hasParent = parent !== null;
 
             /** @constructor */
             var Class = function () {
-                // call the function which is used as a constructor
+                // call the 'init' function which is used as a constructor
                 this.init.apply(this, arguments);
             }
 
-            // inherit
-            if (hasParent) {
-                // basic inherit      
-                Class.prototype = Helper.createObject(parent.prototype);
+            /** function is used as a constructor */
+            Class.prototype.init = function () {}
 
-                // fix the constructor
-                if (Class.prototype.constructor === Object.prototype.constructor) {
-                    Class.prototype.constructor = Class;
-                }
-
-                // keep the superclass reference
-                Class.super = parent.prototype;
-
-                // Class.prototype.parent = Class;
-            }
-
-            // contraction of the prototype
-            Class.fn = Class.prototype;
-
-            // function is used as a constructor
-            Class.fn.init = function () {}
-
+            hasParent && Helper.inherit(parent, Class);
+            Helper.extend(Class.prototype, props);
             return Class;
-        };
-
-        return {
-            /** 
-             * Create a class.
-             * Parameters can be as follows:
-             *   - parent is a parent class
-             *   - props is an object which contains class properties
-             *
-             * @param {function|object|null} The parameter can be parent or props or null. 
-             * @param {object} The parameter is props.
-             * @return {function} A function which can be used as a class.
-             * @throws {TypeError}
-             */
-            create: function () {
-                var wrapper = null,
-                    args = Helper.parseArgs(arguments);
-
-                wrapper = new ClassWrapper(args.parent);
-                Helper.extend(wrapper.fn, args.props);
-                return wrapper;
-            }
-        };
-    })();
+        }
+    };
 })(this);
