68b30ece36d00a4d2f2345bbddaf967bc4342709
Sergey Sorokin <s.sorokin@prodege.com>
2014-04-01 16:50:15 +0400

    fixed tests

--- tests/tests.js
+++ tests/tests.js
@@ -1,3 +1,6 @@
+/**
+ * Helpers functions.
+ */
 var isDefined = function (o) {
     return typeof o !== 'undefined';
 }
@@ -10,104 +13,157 @@ var isObj = function (o) {
     return typeof o === 'object';
 }
 
-
 /**
  * Module: arguments.
  */
-module('arguments', {
-    setup: function () {
-        /**
-         * @param {function} A Class.
-         * @param {object} a Instance of class.
-         */
-        this.checkInstanceAndClassInGeneral = function (A, a) {
-            // type
-            ok(isFn(A), 'type of A is a function');
-            ok(isObj(a), 'type of a is an object');
-
-            // init()
-            ok(isFn(a.init), 'a has the init function');
-            ok(isFn(A.prototype.init), 'function', 'prototype of A has the init function');
-
-            // constructor
-            strictEqual(a.constructor, A, 'constructor of a is A');
-            strictEqual(A.prototype.constructor, A, 'constructor in prototype of A is A (constructor is fixed)');
-        }
-    }
-});
-test('create a class without arguments', function () {
-    var A = Class.create(),
-        a = new A();
-    this.checkInstanceAndClassInGeneral(A, a);
-});
-test('create a class with properties', function () {
-    var A = Class.create({
-        prop: 1
-    });
-    var a = new A();
-    this.checkInstanceAndClassInGeneral(A, a);
-
-    // prop
-    ok(isDefined(a.prop), 'a has prop');
-    ok(!isDefined(A.prop), 'A has not prop');
-    equal(a.prop, 1, 'a.prop has correct value');
-});
-test('create a class with static properties', function () {
-    var A = Class.create({}, {
-        staticProp: 1
-    });
-    var a = new A();
-    this.checkInstanceAndClassInGeneral(A, a);
-
-    // static prop
-    ok(!isDefined(a.staticProp), 'a has not static prop');
-    ok(isDefined(A.staticProp), 'A has static prop');
-    equal(A.staticProp, 1, 'A.staticProp has correct value');
-});
-test('create a class with properties and static properties', function () {
-    var A = Class.create({
-        prop: 1
-    }, {
-        staticProp: 2
-    });
-    var a = new A();
-    this.checkInstanceAndClassInGeneral(A, a);
-
-    // prop
-    ok(isDefined(a.prop), 'a has prop');
-    ok(!isDefined(A.prop), 'A has not prop');
-    equal(a.prop, 1, 'a.prop has correct value');
-
-    // static prop
-    ok(!isDefined(a.staticProp), 'a has not static prop');
-    ok(isDefined(A.staticProp), 'A has static prop');
-    equal(A.staticProp, 2, 'A.staticProp has correct value');
-});
-
-/**
- * Module: inheritance.
- */
-module('inheritance');
-test('inheritance of properties', function () {
+module('arguments');
+test('create class with properties', function () {
     var A = Class.create({
         prop: 100
     });
-    var B = Class.create(A);
-    var b = new B();
 
-    equal(b.prop, 100);
-    ok(!isDefined(B.prop));
+    // prop
+    ok(isDefined(new A().prop));
+    ok(!isDefined(A.prop));
+    equal(new A().prop, 100);
 });
-test('inheritance of methods', function () {
+test('create class with static properties', function () {
+    var A = Class.create({}, {
+        staticProp: 200
+    });
+
+    // static prop
+    ok(!isDefined(new A().staticProp));
+    ok(isDefined(A.staticProp));
+    equal(A.staticProp, 200);
+});
+test('create class with properties and static properties', function () {
     var A = Class.create({
-        prop: 100,
-        getProps: function () {
-            return this.prop;
+        prop: 100
+    }, {
+        staticProp: 200
+    });
+
+    // prop
+    ok(isDefined(new A().prop));
+    ok(!isDefined(A.prop));
+    equal(new A().prop, 100);
+
+    // static prop
+    ok(!isDefined(new A().staticProp));
+    ok(isDefined(A.staticProp));
+    equal(A.staticProp, 200);
+});
+
+/**
+ * Module: general.
+ */
+module('general');
+test('check global variable', function () {
+    ok(isDefined(Class));
+    ok(isFn(Class.create));
+});
+test('create class', function () {
+    var A = Class.create();
+
+    ok(isFn(A));
+    ok(isObj(new A()));
+
+    ok(isFn((new A()).init));
+    ok(isFn(A.prototype.init));
+
+    strictEqual((new A()).constructor, A);
+    strictEqual(A.prototype.constructor, A);
+});
+test('set properties using constructor', function () {
+    var A = Class.create({
+        init: function (v) {
+            this.value = v;
         }
     });
-    var B = Class.create(A);
-    var b = new B();
 
-    equal(b.getProps(), 100);
-    ok(!isDefined(B.getProps));
+    equal((new A('aaa')).value, 'aaa');
+    equal((new A('bbb')).value, 'bbb');
+});
+test('inherit method', function () {
+    var A = Class.create({
+        m1: function () {
+            return 'aaa';
+        }
+    });
+
+    var B = Class.create(A, {
+        m2: function () {
+            return 'bbb';
+        }
+    });
+
+    equal(new B().m1(), 'aaa');
+    equal(new B().m2(), 'bbb');
+
+    ok(isDefined(new A().m1));
+    ok(!isDefined(new A().m2));
+});
+test('override method', function () {
+    var A = Class.create({
+        m1: function () {
+            return 'aaa';
+        }
+    });
+
+    var B = Class.create(A, {
+        /** @override */
+        m1: function () {
+            return 'bbb';
+        }
+    });
+
+    equal(new A().m1(), 'aaa');
+    equal(new B().m1(), 'bbb');
+});
+test('call super() method', function () {
+    var A = Class.create({
+        name: '',
+        init: function (name) {
+            this.name = name;
+        },
+        m1: function () {
+            return this.name;
+        }
+    });
+
+    var B = Class.create(A, {
+        init: function () {
+            this.super.apply(this, arguments);
+        },
+        m1: function () {
+            return this.super() + 'bbb';
+        },
+    });
+
+    var C = Class.create(B, {
+        init: function (name) {
+            this.super(name);
+        },
+        m1: function () {
+            return this.super() + 'ccc';
+        },
+    });
+
+    equal(new C('aaa').m1(), 'aaabbbccc');
+});
+test('call super() method', function () {
+    var A = Class.create({
+        m1: function () {
+            return 'aaa';
+        }
+    });
+
+    var B = Class.create(A, {
+        m1: function () {
+            return B.__super__.m1.apply(this, arguments) + 'bbb';
+        },
+    });
+
+    equal(new B().m1(), 'aaabbb');
 });
